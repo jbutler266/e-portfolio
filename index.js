50d4b35828d1e1afb56b4c0ea258c7be2c17fda5
jbutler266 <josephbutler266@gmail.com>
2025-06-27 00:02:25 -0700

    dark mode

--- index.js
+++ index.js
@@ -2,11 +2,42 @@
 // service id = service_nygorjh
 // public key = l86GM9hvaMZ7D8Fu-
 
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  }
+  else {
+    document.body.classList.remove('dark-theme')
+  }
+}
+
 function contact(event) {
   event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
   emailjs
     .sendForm("service_nygorjh", "template_r8j66r7", event.target)
     .then(() => {
-      console.log("this worked");
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert("The email service is temporarily unavailable.");
     });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
